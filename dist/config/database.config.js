66d4ebffd3e8689bfbc991986b04f22ed567907a
Nguyễn Tuấn Anh <baoawm2@gmail.com>
2026-07-24 23:37:33 +0700

    giới hạn số kết nối MySQL khi triển khai

--- dist/config/database.config.js
+++ dist/config/database.config.js
@@ -2,24 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mysqlConfig = void 0;
 const isMysqlSslEnabled = () => process.env.MYSQL_SSL === 'true';
-const mysqlConfig = () => ({
-    type: 'mysql',
-    host: process.env.MYSQL_HOST ?? 'localhost',
-    port: Number(process.env.MYSQL_PORT ?? 3306),
-    username: process.env.MYSQL_USER ?? 'agile_ai',
-    password: process.env.MYSQL_PASSWORD ?? 'agile_ai',
-    database: process.env.MYSQL_DATABASE ?? 'agile_ai',
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
-    synchronize: false,
-    autoLoadEntities: true,
-    extra: {
-        ssl: isMysqlSslEnabled()
-            ? {
-                rejectUnauthorized: false,
-            }
-            : undefined,
-    },
-});
+const getMysqlConnectionLimit = () => {
+    const configuredLimit = Number(process.env.MYSQL_CONNECTION_LIMIT ?? 2);
+    return Number.isInteger(configuredLimit) && configuredLimit > 0
+        ? configuredLimit
+        : 2;
+};
+const mysqlConfig = () => {
+    const connectionLimit = getMysqlConnectionLimit();
+    return {
+        type: 'mysql',
+        host: process.env.MYSQL_HOST ?? 'localhost',
+        port: Number(process.env.MYSQL_PORT ?? 3306),
+        username: process.env.MYSQL_USER ?? 'agile_ai',
+        password: process.env.MYSQL_PASSWORD ?? 'agile_ai',
+        database: process.env.MYSQL_DATABASE ?? 'agile_ai',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+        poolSize: connectionLimit,
+        extra: {
+            connectionLimit,
+            waitForConnections: true,
+            queueLimit: 0,
+            ssl: isMysqlSslEnabled()
+                ? {
+                    rejectUnauthorized: false,
+                }
+                : undefined,
+        },
+    };
+};
 exports.mysqlConfig = mysqlConfig;
 //# sourceMappingURL=database.config.js.map
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,22 +2,38 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const isMysqlSslEnabled = (): boolean => process.env.MYSQL_SSL === 'true';
 
-export const mysqlConfig = (): TypeOrmModuleOptions => ({
-  type: 'mysql',
-  host: process.env.MYSQL_HOST ?? 'localhost',
-  port: Number(process.env.MYSQL_PORT ?? 3306),
-  username: process.env.MYSQL_USER ?? 'agile_ai',
-  password: process.env.MYSQL_PASSWORD ?? 'agile_ai',
-  database: process.env.MYSQL_DATABASE ?? 'agile_ai',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
-  synchronize: false,
-  autoLoadEntities: true,
-  extra: {
-    ssl: isMysqlSslEnabled()
-      ? {
-          rejectUnauthorized: false,
-        }
-      : undefined,
-  },
-});
+const getMysqlConnectionLimit = (): number => {
+  const configuredLimit = Number(process.env.MYSQL_CONNECTION_LIMIT ?? 2);
+
+  return Number.isInteger(configuredLimit) && configuredLimit > 0
+    ? configuredLimit
+    : 2;
+};
+
+export const mysqlConfig = (): TypeOrmModuleOptions => {
+  const connectionLimit = getMysqlConnectionLimit();
+
+  return {
+    type: 'mysql',
+    host: process.env.MYSQL_HOST ?? 'localhost',
+    port: Number(process.env.MYSQL_PORT ?? 3306),
+    username: process.env.MYSQL_USER ?? 'agile_ai',
+    password: process.env.MYSQL_PASSWORD ?? 'agile_ai',
+    database: process.env.MYSQL_DATABASE ?? 'agile_ai',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+    synchronize: false,
+    autoLoadEntities: true,
+    poolSize: connectionLimit,
+    extra: {
+      connectionLimit,
+      waitForConnections: true,
+      queueLimit: 0,
+      ssl: isMysqlSslEnabled()
+        ? {
+            rejectUnauthorized: false,
+          }
+        : undefined,
+    },
+  };
+};
